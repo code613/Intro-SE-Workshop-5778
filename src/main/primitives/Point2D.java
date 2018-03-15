e6c77b02a6dd3c60645a21f78dff79cd94e18138
eliezergens <eliezer.jct@gmail.com>
2018-03-15 19:17:48 +0200

    1st commit

--- src/main/primitives/Point2D.java
+++ src/main/primitives/Point2D.java
@@ -1,4 +1,23 @@
 package main.primitives;
 
 public class Point2D {
+    private Coordinate _x;
+    private Coordinate _y;
+
+    public Coordinate get_X() {
+        return new Coordinate(_x.get_coordinate());
+    }
+
+    public void set_X(Coordinate x) {
+        this._x = new Coordinate(x.get_coordinate());
+    }
+
+    public Coordinate get_Y() {
+        return new Coordinate(_y.get_coordinate());
+    }
+
+    public void set_Y(Coordinate y) {
+        this._y = new Coordinate(y.get_coordinate());
+    }
+
 }
